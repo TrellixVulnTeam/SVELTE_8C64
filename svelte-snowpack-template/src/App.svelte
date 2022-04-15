c87cbedef31f36b88a4587a300f3bd50904b14c7
miiin_sseong <wsc03002@naver.com>
2022-04-15 16:08:35 +0900

    test

--- svelte-snowpack-template/src/App.svelte
+++ svelte-snowpack-template/src/App.svelte
@@ -2,4 +2,14 @@
     let name = 'Hello Svelte'
 </script>
 
+<style lang="scss">
+    $color: red;
+    h1{
+        color: $color
+    }
+    .minseong{
+        color: red;
+    }
+</style>
+
 <h1>{name}</h1>
